fc8824d0c07db2c91ccfd0b168a34699472d7e3c
EUNGWANCHOI <silvergwan@gmail.com>
2025-11-11 11:19:41 +0900

    feat :: server full code(서버 풀 코드)

--- serverSocket.py
+++ serverSocket.py
@@ -1,7 +1,36 @@
 from socket import *
+import threading
+import time
+
+def send(sock):
+    while True:
+        msg = input(">>> ")
+        sock.send(msg.encode('utf-8'))
+
+def receive(sock):
+    while True:
+        recvData = sock.recv(1024)
+        print("상대방 :", recvData.decode('utf-8'))
+
+port = 8081
+
 
 serverSock = socket(AF_INET, SOCK_STREAM) #소켓 객체 생성
-serverSock.bind(('', 8080)) #포트 바인딩
+serverSock.bind(('', port)) #포트 바인딩
 serverSock.listen(1) #클라이언트의 접속을 기다림
 
+print("%d번 포트에서 접속 대기중..." %port)
+
 connestionSock, addr = serverSock.accept() #클라이언트 접속 허용
+
+print(str(addr), "에서 접속하였습니다.")
+
+sender = threading.Thread(target=send, args=(connestionSock,))
+receiver = threading.Thread(target=receive, args=(connestionSock,))
+
+sender.start()
+receiver.start()
+
+while True:
+    time.sleep(1)
+    pass
