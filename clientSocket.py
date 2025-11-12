8561c47f98e24ee6012d2ccb83abd7720c7bf87e
EUNGWANCHOI <silvergwan@gmail.com>
2025-11-12 17:44:37 +0900

    feat :: add client socket(클라이언트 소켓)

--- clientSocket.py
+++ clientSocket.py
@@ -2,5 +2,29 @@ from socket import *
 import threading
 import time
 
+def send(sock):
+    while True:
+        sendData = input('>>>')
+        sock.send(sendData.encode('utf-8'))
+
+def receive(sock):
+    while True:
+        recvData = sock.recv(1024)
+        print('상대방 :', recvData.decode('utf-8'))
+
+port = 8081
+
 clientSock = socket(AF_INET, SOCK_STREAM) #소켓 객체 생성
-clientSock.connect(('localhost', 8080)) #서버에 접속
+clientSock.connect(('localhost', 8081)) #서버에 접속
+
+print('접속완료')
+
+sender = threading.Thread(target=send, args=(clientSock,))
+receiver = threading.Thread(target=receive, args=(clientSock,))
+
+sender.start()
+receiver.start()
+
+while True:
+    time.sleep(1)
+    pass
